b3cb52ea62a286e2b036d300b31fb20a5236f22e
Jingwen Xia <amyfor7@gmail.com>
2022-12-09 00:58:39 +0800

    test: refactor test

--- app/src/test/java/tw/cn/gtb/AppTest.java
+++ app/src/test/java/tw/cn/gtb/AppTest.java
@@ -2,6 +2,7 @@ package tw.cn.gtb;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -29,17 +30,29 @@ class AppTest {
         }
     }
 
-    @Test
-    void should_show_all_tasks() {
-        App app = new App();
-        List<String> res = app.run();
-        Assertions.assertEquals(List.of("#To be done", "Empty", "#Completed", "1 go to walk", "2 count stars"), res);
+    @Nested
+    class ListCommand {
+        @Nested
+        class ListTasksWhenExists {
+            @Test
+            void should_show_tasks_when_task_exists() {
+                App app = new App();
+                List<String> res = app.run();
+                Assertions.assertEquals(List.of("#To be done", "Empty", "#Completed", "1 go to walk", "2 count stars"), res);
+            }
+        }
     }
 
-    @Test
-    void should_add_task_with_single_word_as_name() {
-        new App().run("add", "rush");
-        List<String> res = new App().run();
-        Assertions.assertEquals(List.of("#To be done", "3 rush", "#Completed", "1 go to walk", "2 count stars"), res);
+    @Nested
+    class AddCommand {
+        @Nested
+        class AddTaskGivenSingleName {
+            @Test
+            void should_add_task_with_single_word() {
+                new App().run("add", "rush");
+                List<String> res = new App().run();
+                Assertions.assertEquals(List.of("#To be done", "3 rush", "#Completed", "1 go to walk", "2 count stars"), res);
+            }
+        }
     }
 }
